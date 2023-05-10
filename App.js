a6320a912c7f40fa37121c9d6a04740933bfb3d5
billywba <billy.wba8@gmail.com>
2023-05-10 15:40:04 +0100

    feature/homepage: added button

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontWeight: 'bold', fontFamily: 'Arial', fontSize: 24 }}>DEEPCONVCHESS</Text>
+      <Text style={{ fontWeight: 'bold', fontFamily: 'Arial', fontSize: 36 }}>DEEPCONVCHESS</Text>
+
+      <TouchableOpacity>
+        <Text style={styles.button}>SCAN A BOARD</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +21,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: '#d9d9d9',
+    color: 'black',
+    fontSize: 20,
+    padding: 10,
+    fontWeight: 'bold'
+  }
 });
